a1c43efe94953d9192f8f8550ef5862d1189670c
Kristofers Solo <kristians.cagulis@gmail.com>
2021-10-21 17:16:25 +0300

    gitignore update

--- kristofers/test/Game_2/forest/source/full forest.gd
+++
@@ -1,7 +0,0 @@
-extends Spatial
-
-onready var anim = $AnimationPlayer
-
-func _ready():
-	anim.get_animation("Take 001").set_loop(true)
-	anim.play("Take 001")
--- kristofers/test/Game_3/singleton.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var user_id = -1
-
-func _ready():
-	pass
