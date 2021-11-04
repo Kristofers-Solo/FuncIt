b03cb11620d4cd59b8e7e05abf9a4bd7e5f8e820
Alan Alexander Cerna <87852561+Kroppec@users.noreply.github.com>
2021-11-04 14:12:08 +0200

    [Minimal] Animation fix - Grass node

--- Game/source/assets/scripts/grass_handlers/grass_body.gd
+++ Game/source/assets/scripts/grass_handlers/grass_body.gd
@@ -1,16 +1,44 @@
 extends Area2D
 
 var reacted = false
+var reactionComplete = true
+var state = false
+var activeAnimation = "idle"
+var playingLoop = true
+
+func _process(delta):
+	if reacted == true:
+		playingLoop = false
+		reactionComplete = false
+		if state == false:
+			activeAnimation = "transition_down"
+			reacted = false
+			state = true
+		else:
+			activeAnimation = "transition_up"
+			reacted = false
+			state = false
+	else:
+		if reactionComplete == true:
+			if state == false:
+				activeAnimation = "idle"
+			else:
+				activeAnimation = "idle_down"
+	print(reactionComplete)
 
 func _on_Area2D_body_entered(body):
-	print("Entered")
 	if body.is_in_group("Player"):
-		$trinity_site_level_grass_type_2_animated.play("react")
 		reacted = true
 	pass 
 
 func _on_Area2D_body_exited(body):
-	if body.is_in_group("Player") and reacted == true:
-		$trinity_site_level_grass_type_2_animated.play("idle")
-		reacted = false
+	if body.is_in_group("Player"):
+		reacted = true
 	pass
+
+func _on_trinity_site_level_grass_type_2_animated_animation_finished():
+	if reactionComplete == false:
+		reactionComplete = true
+	$trinity_site_level_grass_type_2_animated.play(activeAnimation)
+	pass
+	
