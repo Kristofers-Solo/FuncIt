023a86f22615debd9b3fd5f548f7747760ad6080
Kristofers Solo <kristofers.solo@gmail.com>
2021-11-08 14:11:12 +0200

    Deleted individual projects

--- Alens/movementSystem0.0.2/root/levels/testing_grounds/windowController.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	var windowExpectedWidth = $StaticBody2D/terrain_sprite.texture.get_width() * $StaticBody2D/terrain_sprite.scale.x
-	var windowExpectedHeight = $StaticBody2D/terrain_sprite.texture.get_height() * $StaticBody2D/terrain_sprite.scale.y
-	OS.set_window_size(Vector2(windowExpectedWidth, windowExpectedHeight))
-	pass
