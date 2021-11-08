002d67968bd09c780d5b01e97d5f5cddb1f21236
Alan Alexander Cerna <87852561+Kroppec@users.noreply.github.com>
2021-11-09 00:30:10 +0200

    [Minimal] Fixed username rotation

--- source/assets/scripts/ui_element_handlers/username_text.gd
+++ source/assets/scripts/ui_element_handlers/username_text.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 var player_following = null
 var text = "" setget text_set
+
+var distanceToPlayerOffset = -100
+
 onready var label = $Label
 
 
 func _process(delta: float) -> void:
 	if player_following != null:
-		global_position = player_following.global_position
+		print(player_following.global_position)
+		global_position = player_following.global_position + Vector2(0,distanceToPlayerOffset).rotated(player_following.rotation)
 
 
 func text_set(new_text) -> void:
