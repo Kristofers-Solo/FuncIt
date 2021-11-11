da9047544d3b6e04e0900b7ebe00d8a4935da74e
Kristofers Solo <kristofers.solo@gmail.com>
2021-11-12 01:12:34 +0200

    Changed trinity site .png files to .svg

--- source/assets/scripts/trinity_site_body_handler.gd
+++ source/assets/scripts/trinity_site_body_handler.gd
@@ -1,7 +1,9 @@
 extends StaticBody2D
 
+onready var clouds = $scene/trinity_site_level_layout_level_design_z_index_1
+
 func _process(delta):
-	if $"trinity_site_level_layout-level_design_z-index_-2-version_ct_f_hd_3840_2160 – 01/trinity_site_level_layout-level_design_z-index_-2-version_ct_f_hd_3840_2160 – 025".rotation_degrees < -58:
-		$"trinity_site_level_layout-level_design_z-index_-2-version_ct_f_hd_3840_2160 – 01/trinity_site_level_layout-level_design_z-index_-2-version_ct_f_hd_3840_2160 – 025".rotation_degrees = 0
-	$"trinity_site_level_layout-level_design_z-index_-2-version_ct_f_hd_3840_2160 – 01/trinity_site_level_layout-level_design_z-index_-2-version_ct_f_hd_3840_2160 – 025".rotation_degrees += -0.01
+	if clouds.rotation_degrees < -58:
+		clouds.rotation_degrees = 0
+	clouds.rotation_degrees += -0.01
 	pass
