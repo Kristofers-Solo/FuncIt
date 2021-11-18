be143754beb7de001a912d27d0b7cb32da1bf546
Alan Alexander Cerna <87852561+Kroppec@users.noreply.github.com>
2021-11-18 17:57:17 +0200

    [Medium] Alena f'up.

---
+++ source/assets/scripts/server_handlers/network_processors/singleplayer_setup_tutorial.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var player = load("res://source/entities/player/player_node.tscn")
+
+func _ready():
+	Global.alive_players.append(self)
+	Network.current_player_username = "You"
+	Network.create_server()
+	instance_player(get_tree().get_network_unique_id())
+	rpc("switch_to_game")
+
+
+func instance_player(id) -> void:
+	var player_instance = Global.instance_node_at_location(player, PersistentNodes, Vector2())
+	player_instance.name = str(id)
+	player_instance.set_network_master(id)
+	player_instance.username = "You"
+
+sync func switch_to_game() -> void:
+	for child in PersistentNodes.get_children():
+		if child.is_in_group("Player"):
+			child.update_shoot_mode(true)
+	get_tree().change_scene("res://source/scenes/GAME/game_tutorial.tscn")
--- source/assets/scripts/server_handlers/trinity_site_level_playground.gd
+++ source/assets/scripts/server_handlers/trinity_site_level_playground.gd
@@ -5,18 +5,8 @@ var current_player_location_instance_number = null
 
 var globalActivePhase = null
 
-var timer = 0
-var finishedMovementZone = false
-var finishedJumpZone = false
-var finishedAiming = false
-var botCount = 0
-
-var ts_bot = preload("res://source/entities/ts_bot/ts_bot.tscn")
-
 func _ready() -> void:
-	$controls/timer/phase.text = "Tutorial"
 # warning-ignore:return_value_discarded
-	$controls.modulate[3] = 0
 	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
 	if get_tree().is_network_server():
 		setup_player_positions()
@@ -36,41 +26,3 @@ func _player_disconnected(id) -> void:
 		PersistentNodes.get_node(str(id)).username_text_instance.queue_free()
 		PersistentNodes.get_node(str(id)).health_bar_instance.queue_free()
 		PersistentNodes.get_node(str(id)).queue_free()
-
-func _process(delta):
-	timer += delta
-	begin_tutorial()
-
-func begin_tutorial():
-	# Reset initial setup
-	if timer < 2:
-		finishedJumpZone = false
-		finishedMovementZone = false
-	# Request to start tutorial.
-	# Show a movement target to test ( A / D / SHIFT )
-	if not finishedMovementZone: $simpleTargetZone_basic.show()
-	else: $simpleTargetZone_basic.hide()
-	if not finishedJumpZone and finishedMovementZone: $simpleTargetZone_jump.show()
-	else: $simpleTargetZone_jump.hide()
-	if finishedJumpZone and finishedMovementZone and $controls.modulate[3] < 1: 
-		$controls.modulate[3] += 0.1
-		$weaponInstruction.show()
-		if botCount < 6:
-			var bot = ts_bot.instance()
-			add_child(bot)
-			bot.global_position = $bot_spawn_locations.get_child(botCount).global_position
-			botCount += 1
-	# After target destroyed request permission to move on to menu.
-	# Remove player before moving on. !!!!!!!!!!!
-	
-	if finishedMovementZone and finishedJumpZone and finishedAiming and timer > 2: 
-		Network._server_leave()
-		get_tree().change_scene("res://source/scenes/GUI/main_menu.tscn")
-
-
-func _on_simpleTargetZone_jump_zone_entered():
-	finishedJumpZone = true
-
-
-func _on_simpleTargetZone_basic_zone_entered():
-	finishedMovementZone = true
