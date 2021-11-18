a1f461427c06dba865c9c0574056f938208aefa5
Alan Alexander Cerna <87852561+Kroppec@users.noreply.github.com>
2021-11-18 13:48:08 +0200

    [Minimal] Merge fixes

--- source/assets/scripts/server_handlers/network_processors/singleplayer_setup.gd
+++ source/assets/scripts/server_handlers/network_processors/singleplayer_setup.gd
@@ -2,24 +2,12 @@ extends Control
 
 var player = load("res://source/entities/player/player_node.tscn")
 
-func _ready() -> void:
-	username_text_edit.call_deferred("grab_focus")
-
-
-func _process(_delta) -> void:
-	if Input.is_action_just_pressed("esc"):
-# warning-ignore:return_value_discarded
-		get_tree().change_scene("res://source/scenes/GUI/main_menu.tscn")
-	if Input.is_action_just_pressed("enter") and username_text_edit.is_visible_in_tree():
-		_on_confirm_pressed()
-
-func _on_confirm_pressed():
-	if username_text_edit.text != "":
-		Global.alive_players.append(self)
-		Network.current_player_username = username_text_edit.text
-		Network.create_server()
-		instance_player(get_tree().get_network_unique_id())
-		rpc("switch_to_game")
+func _ready():
+	Global.alive_players.append(self)
+	Network.current_player_username = "You"
+	Network.create_server()
+	instance_player(get_tree().get_network_unique_id())
+	rpc("switch_to_game")
 
 
 func instance_player(id) -> void:
